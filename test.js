7bb3c3c3158e8bd85d985bf1519e98ee7f672bc4
Matt Joiner <mdjoiner@benekeith.com>
2018-10-24 11:18:22 -0500

    [#12346] - Adding a parameter to helloWorld to make it more personalized

--- test.js
+++ test.js
@@ -2,6 +2,6 @@
     A simple test of the Git conflict and merge systems
 */
 
-function helloWorld() { 
-    Console.Log("Hello World!");
+function helloWorld(nameOfPerson) { 
+    Console.Log("Hello " + nameOfPerson);
 }
